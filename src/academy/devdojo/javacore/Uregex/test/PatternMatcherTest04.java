cb03354588688431f09414ae4bf3554f2c909093
JuliaSPalmeira <serennamares@gmail.com>
2025-12-22 22:42:34 -0300

    regex

--- src/academy/devdojo/javacore/Uregex/test/PatternMatcherTest04.java
+++ src/academy/devdojo/javacore/Uregex/test/PatternMatcherTest04.java
@@ -3,7 +3,7 @@ package academy.devdojo.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest03 {
+public class PatternMatcherTest04 {
     public static void main(String[] args) {
         // mias basicos -> \d = todos os digitos
         // \\D = tudo que não for digito (letras e caracteres)
@@ -15,13 +15,20 @@ public class PatternMatcherTest03 {
         //  String regex = "abc"; é essa exata sequencia
         // AZ min e max
         //hexa decimal valido 0x
+        //? Zero ou uma
+        //* Zero ou mais
+        //+ uma ou mais
+        //{n,m} de n até m
+        // agrupamento ()
+        // ou | o(v|c)0 = ovo ou oco
+        //$ fim da linha
 
-     //       String regex = "[a-zA-C]";
-        String regex = "0[xX][0-9a-fA-F]"; //[0-9] ou [\\d]
-        String texto2 = "12 0x 0x 0xFFABC 0x109 0x1";
+
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)"; //[0-9] ou [\\d]
+        String  texto = "12 0x 0x 0xFFABC 0x10G 0x1";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(texto2);
-        System.out.println("Texto:   "+texto2);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto:   "+texto);
         System.out.println("Indice:  0123456789");
         System.out.println("regex:  "+regex);
         System.out.println("Posições encontradas");
--- src/academy/devdojo/javacore/Uregex/test/PatternMatcherTest05.java
+++ src/academy/devdojo/javacore/Uregex/test/PatternMatcherTest05.java
@@ -3,7 +3,7 @@ package academy.devdojo.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest03 {
+public class PatternMatcherTest05 {
     public static void main(String[] args) {
         // mias basicos -> \d = todos os digitos
         // \\D = tudo que não for digito (letras e caracteres)
@@ -15,13 +15,23 @@ public class PatternMatcherTest03 {
         //  String regex = "abc"; é essa exata sequencia
         // AZ min e max
         //hexa decimal valido 0x
+        //? Zero ou uma
+        //* Zero ou mais
+        //+ uma ou mais
+        //{n,m} de n até m
+        // agrupamento ()
+        // ou | o(v|c)0 = ovo ou oco
+        //$ fim da linha
+        //coringa . 1.3 -> 123, 133, 1@3 entre 1 e 3 ele é considerado
 
-     //       String regex = "[a-zA-C]";
-        String regex = "0[xX][0-9a-fA-F]"; //[0-9] ou [\\d]
-        String texto2 = "12 0x 0x 0xFFABC 0x109 0x1";
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"; //se repita + uma ou mais
+        String  texto = "luffy@hotmail.com, 123jotaro@gmail.com,#@!zoro@mail.br, teste@gmail.com.br, sakura@mail";
+        System.out.println("Email valido");
+        System.out.println("#@!zoro@mail.br".matches(regex)); // bate a strign com a expressão regular
+        System.out.println(texto.split(",")[1].trim());
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(texto2);
-        System.out.println("Texto:   "+texto2);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("Texto:   "+texto);
         System.out.println("Indice:  0123456789");
         System.out.println("regex:  "+regex);
         System.out.println("Posições encontradas");
@@ -31,4 +41,8 @@ public class PatternMatcherTest03 {
         int numHex = 0x1; //numero decimal valido
 //        System.out.println(numHex);// mostra o numero decimal de fato
     }
+    //como encontrar todos os emails validos
+    // \\ para usar caracter de escape da expressão regular
+    //validando padrão n o texto e n uma determinada string
+    // no regex o + pleo men  os 1 e mais ou seja se n tiver ele covai ficar .c
 }
