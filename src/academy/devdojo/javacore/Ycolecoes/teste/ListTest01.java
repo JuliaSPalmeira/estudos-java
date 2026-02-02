69f0bea123835b64a8f4e70fec2d866de401016b
JuliaSPalmeira <serennamares@gmail.com>
2026-02-02 20:11:30 -0300

    começo de list

---
+++ src/academy/devdojo/javacore/Ycolecoes/teste/ListTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.javacore.Ycolecoes.teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List <String> nomes = new ArrayList<>(16);
+        nomes.add("William");
+        nomes.add("DevDojo Academy");
+
+        for(String nome: nomes){
+            System.out.println(nome);
+        }
+
+
+        nomes.add("Suane");
+        System.out.println("----------");
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println(nomes.get(i));
+        }
+    }
+}
