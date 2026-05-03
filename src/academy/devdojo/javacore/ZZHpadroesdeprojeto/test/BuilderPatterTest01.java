82ab0971ba914f7db348b995ae7ecaeeec55de6c
JuliaSPalmeira <serennamares@gmail.com>
2026-05-02 23:28:27 -0300

    build

---
+++ src/academy/devdojo/javacore/ZZHpadroesdeprojeto/test/BuilderPatterTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.javacore.ZZHpadroesdeprojeto.dominio.Person;
+
+
+public class BuilderPatterTest01 {
+    public static void main(String[] args) {
+        Person build = new Person.PersonBuilder()
+                .firstName("Julia")
+                .lastName("plameira")
+                .userName("whitefox")
+                .email("juliapalmeira@gmail.com")
+                .build();
+        System.out.println(build);
+
+    }
+}
